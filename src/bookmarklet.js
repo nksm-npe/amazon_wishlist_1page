608b498b8f4a50d73fd04d8660dbfff87ac2627b
nksm-npe <notpoorelephant@gmail.com>
2013-06-15 17:29:41 +0900

    mod bookmarklet.js

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -3,8 +3,6 @@ javascript:
   var d = document;
   var s = d.createElement('script');
   s.src = 
-//  'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js';
-//  'http://localhost:8080/test.js';
-    'https://raw.github.com/nksm-npe/amazon_wishlist_1page/master/src/test.js';
+  'http://amazonwishlist.web.fc2.com/test.js';
   d.body.appendChild(s)
 })();
--- src/test.js
+++ src/test.js
@@ -11,23 +11,37 @@ function call_jquery(){
   d.body.appendChild(s)
 };
 call_jquery();
+
+
 //--------------------------------------//
 //   putItemsToNewWindow()
 //--------------------------------------//
 function putItemsToNewWindow(nw){
-	console.log('putItemsToNewWindow');
 	var items;
 	//itemを配列として取得する。 一秒ディレイを入れている
-	console.log('putItemsToNewWindow2');
-	setTimeout(function(){console.log('dummy');},1000);
+	setTimeout(function(){console.log('dummy' + j);},1000);
 	items = $('tbody.itemWrapper');
-	console.log('putItemsToNewWindow3');
 	console.log(items.length);
 	//新しいウィンドウにitemをhtmlとして追加する
 	for(var i = 0; i < items.length;i++){   
-	console.log('putItemsToNewWindowLOOP');
-		    nw.body.insertAdjacentHTML('afterend',items[i].outerHTML);
+	    nw.body.insertAdjacentHTML('afterend',items[i].outerHTML);
 	}
+}
+
+
+// Groval Variable 
+var url_pre="";
+var url_post="";
+//--------------------------------------//
+//   makeUrlParts();
+//--------------------------------------//
+function makeUrlParts(){
+    var url1strt = 0
+    var url1length = pagdiv_a[2].href.indexOf('_page_',0)+6;
+    var url2strt = url1length+1
+    var url2length = pagdiv_a[2].href.indexOf('page=',0)+5 - url2strt;
+    url_pre = pagdiv_a[2].href.substr(url1strt,url1length);
+    url_post = pagdiv_a[2].href.substr(url2strt,url2length);
 }
 
 //--------------------------------------//
@@ -37,19 +51,23 @@ function putItemsToNewWindow(nw){
 
 //新しいウィンドウを作る
 nw = window.open().document;
-console.log('ok');
 //ページリンクを取得する
 pagdiv_a = $('div.pagDiv a');
+
+//Url用文字列部品を作る。
+makeUrlParts();
 
 //最終ページ番号を取得する
 var lastpagenum = pagdiv_a[pagdiv_a.length-2].attributes.getNamedItem("page").nodeValue;
 console.log(lastpagenum);
-for(var j=2 ;j <= lastpagenum ; j++){
-	console.log(j);
-	pagdiv_a = $('div.pagDiv a');
-	console.log('before pIT');
-	setTimeout(console.log('main_setTimeout'),3000);
-	call_jquery();
-	pagdiv_a[j].click;
-	putItemsToNewWindow(nw);
+for(var pageNum=2 ;pageNum <= lastpagenum ; pageNum++){
+	console.log(pageNum);
+    url = url_pre + pageNum + url_post + pageNum;
+	console.log(url);
+	document.location = url;
+	$(function(){
+	    setTimeout('console.log("main_setTimeout")',3000);
+	    call_jquery();
+	    putItemsToNewWindow(nw);
+	});
 }
